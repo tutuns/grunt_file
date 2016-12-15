e5cfa7db7713ab37c1467eee3936875838abd9af
tutun\Tutun Sikari <tutunshk@gmail.com>
2016-12-15 11:59:21 -0800

    final grunt contrib connect working

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,84 +4,30 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     //connect
-
-    //jshint
-    jshint : {
-    	files: ['./src/js/*.js'],
-		options: {
-		    "curly": true,
-		    "eqnull": true,
-		    "eqeqeq": true,
-		    "undef": true,
-		    "globals": { "jQuery": true },
-		    reporter: require('jshint-stylish'),
-
+    connect: {
+		server: {
+			options: {
+				port: 9000,
+				base: 'src',
+				hostname: 'localhost', 
+				livereload: 35729,
+				keepalive: true,
+				open: {
+					target: 'http://localhost:9000'
+				}
+			}		
 		}
 	},
-    //concat
-	concat: {
-		options: {
-		  separator: ';',
-	      banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */',
-		},
-		dist: {
-		  src: ['src/js/header.js', 'src/js/sidebar.js', 'src/js/popup.js'],
-		  dest: 'src/js/common.js',
-		},
-	},
-	//sass
-	sass: {
-		dist: {
-		  files: [{
-		    expand: true,
-		    cwd: 'styles',
-		    src: ['src/scss/*.scss'],
-		    dest: '../css',
-		    ext: '.css'
-		  }]
-		}
-	},
-	//watch
-	watch: {
-	  scripts: {
-	    files: ['src/js/**/*.js', 'src/js/*.js'],
-	    tasks: ['jshint'],
-	    options: {
-	      spawn: false,
-	      interrupt: true,
-	      event: ['all', 'changed', 'added', 'deleted'],
-	      reload: true,
-	      forever: true,
-	      livereload: true,
-	    },
-	  },
 
-	  css: {
-	    files: ['src/scss/**/*.scss', 'src/scss/*.scss'],
-	    tasks: ['sass'],
-	    options: {
-	      spawn: false,
-	      interrupt: true,
-	      event: ['all', 'changed', 'added', 'deleted'],
-	      reload: true,
-	      forever: true,
-	      livereload: true,
-	    },
-	  },
-
-	},
   });
 
 
 
 
   // Load the plugin that provides the "jshint" task
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'concat', 'watch']); //'sass' should goes in 3rd position,
+  grunt.registerTask('default', ['connect']); //'sass' should goes in 3rd position,
 
 }
--- dist/js/common.js
+++
@@ -1,1 +0,0 @@
-/*! grunt_file - v1.0.0 - 2016-12-15 *///header.js;//sidebar.js;//popup.js
